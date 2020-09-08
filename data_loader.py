7ce41b896965efe53ec8d61e8fb321c2195c1b6a
casperbh96 <casperbh.96@gmail.com>
2020-09-08 12:42:02 +0200

    Loaded tasks and mcp+cores into pandas dataframes

--- data_loader.py
+++ data_loader.py
@@ -1,4 +1,5 @@
 import xmltodict
+import pandas as pd
 
 class DataLoader():
     def __init__(self):
@@ -11,10 +12,41 @@ class DataLoader():
         with open(file_path, 'r') as file:
             data = file.read()
             dictionary = xmltodict.parse(data)
-            return dotdict(dictionary)
 
-class dotdict(dict):
-    """dot.notation access to dictionary attributes"""
-    __getattr__ = dict.get
-    __setattr__ = dict.__setitem__
-    __delattr__ = dict.__delitem__
+            return dictionary['Model']
+
+    def get_mcp_to_dataframe(self, mcps: dict) -> pd.DataFrame:
+        core_ids = []
+        core_mcp_ids = []
+        core_wcet_factors = []
+
+        for mcp in mcps:
+            for core in mcp['Core']:
+                core_ids.append(core['@Id'])
+                core_mcp_ids.append(mcp['@Id'])
+                core_wcet_factors.append(core['@WCETFactor'])
+
+        return pd.DataFrame({
+            'CoreId':core_ids,
+            'CoreMcpId':core_mcp_ids,
+            'CoreWcetFactor':core_wcet_factors
+        })
+
+    def get_tasks_to_dataframe(self, tasks: dict) -> pd.DataFrame:
+        ids = []
+        deadlines = []
+        periods = []
+        wcets = []
+
+        for task in tasks:
+            ids.append(task['@Id'])
+            deadlines.append(task['@Deadline'])
+            periods.append(task['@Period'])
+            wcets.append(task['@WCET'])
+
+        return pd.DataFrame({
+            'Id':ids,
+            'Deadline':deadlines,
+            'Period':periods,
+            'WCET':wcets
+        })
--- main.py
+++ main.py
@@ -2,5 +2,15 @@ from data_loader import DataLoader
 
 loader = DataLoader()
 
-data = loader.load_xml_by_name('small')
-print(data['Model'])
+# dict/dictionary == hashtable
+dictionary = loader.load_xml_by_name('small')
+
+task_df = loader.get_tasks_to_dataframe(
+    dictionary['Application']['Task']
+)
+
+mcp_core_df = loader.get_mcp_to_dataframe(
+    dictionary['Platform']['MCP']
+)
+
+print(mcp_core_df.head(5))
