ac92d10dd4ff38bd6ae9c2210b4b842d3cef3760
alpealbert <olivernielsen@msn.com>
2020-11-24 10:17:39 +0100

    generate_neighbor implemented with 4-opt neighbor swap

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 loader = DataLoader()
 
 # dict/dictionary == hashtable
-dictionary = loader.load_xml_by_name('small')
+dictionary = loader.load_xml_by_name('large')
 
 task_df = loader.get_tasks_to_dataframe(
     dictionary['Application']['Task']
@@ -18,8 +18,8 @@ mcp_core_df = loader.get_mcp_to_dataframe(
 annealing = SimulatedAnnealing()
 dm = DeadlineMonotic()
 
-temp = 10000
-temp_decrease = 0.003
+temp = 1000
+temp_decrease = 0.001
 c = annealing.initial_candidate(task_df, mcp_core_df)
 list_of_schedules = [c]
 
